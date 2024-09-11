37f2c55f165ad7466f391ed6dba2499e12a5059c
amdeering <amanda.deering@portlandoregon.gov>
2024-09-11 11:08:06 -0700

    introducting pipes

--- R/intro-libraries-imports.R
+++ R/intro-libraries-imports.R
@@ -17,7 +17,26 @@ nrow(sta_meta) #number of rows
 summary(sta_meta) #gives NA counts, which can be helpful to check import
 
 #reading in excel files
+#uses a specific package
 #used the Import feature to read in xlsx and copy/paste the code below
 library(readxl)
 icebreaker_answers <- read_excel("data/icebreaker_answers.xlsx")
 View(icebreaker_answers)
+
+#entering the tidyverse
+library(dplyr)
+#gives a warning that some of the functions in this package override some same-
+#named ones in other base packages, when calling by name
+
+odot_meta <- sta_meta |>
+  filter(agency == "ODOT",
+         highwayid == 1)
+
+notodot_meta <- sta_meta |>
+  filter(agency != "ODOT")
+#looking for NAs
+nas_meta <- sta_meta |>
+  filter(is.na(detectorlocation))
+#excluding NAs
+real_meta <- sta_meta |>
+  filter(!is.na(detectorlocation))
