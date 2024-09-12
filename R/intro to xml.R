438c4d087138e1be99faa4034e436997fe5bf3d9
amdeering <amanda.deering@portlandoregon.gov>
2024-09-12 16:37:25 -0700

    intro to xml

---
+++ R/intro to xml.R
@@ -0,0 +1,17 @@
+####intro to xml####
+#R class day 3
+#special session
+
+#load library
+library(tidyr)
+library(dplyr)
+library(xml2)
+library(readr)
+
+#read in xml data for wsdot stations metadata
+meta_xml <- as_list(read_xml("https://wsdot.wa.gov/Traffic/WebServices/SWRegion/Service.asmx/GetRMDCLocationData"))
+
+meta_df <- as_tibble(meta_xml) %>% 
+  unnest_longer(RTDBLocationList)
+
+
